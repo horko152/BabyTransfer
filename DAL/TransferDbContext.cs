361a40a860f730c1a07ba8fb7905a5262a6833a1
Igor Voloshenyuk <32906067+horko152@users.noreply.github.com>
2020-06-26 16:12:13 +0300

    Update TransferDbContext.cs

--- DAL/TransferDbContext.cs
+++ DAL/TransferDbContext.cs
@@ -24,7 +24,7 @@ namespace DAL
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer(@"Host=ec2-54-246-85-151.eu-west-1.compute.amazonaws.com; Port=5432; Database=d1ev4oevtggnb4; Username=asntnkxuaztbvo; Password = 9c9f8f3faa8054d8f8abfdca04a9375b1c03f574ed9cc7b4114dc43aa1da95e4; SSL Mode = Require; TrustServerCertificate = True", builder =>
+			optionsBuilder.UseSqlServer(@"", builder =>
 			{
 				builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
 			});
